04952a43cc9d44d1f460d0662638097babdc9a8d
Adan <programmerAdan@gmail.com>
2018-10-24 19:55:56 -0500

    Adding Content folders FPS and Effects

--- Source/MyProject2/Private/Characters/PlayerCharacter.cpp
+++ Source/MyProject2/Private/Characters/PlayerCharacter.cpp
@@ -1,10 +1,76 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayerCharacter.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 
 APlayerCharacter::APlayerCharacter() 
 {
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 
+	if (SpringArm)
+	{
+		SpringArm->SetupAttachment(GetCapsuleComponent());
+		SpringArm->RelativeLocation = FVector(-20.0f, 0.0f, 40.0f);
+		SpringArm->TargetArmLength = 0.0f;
+		SpringArm->bDoCollisionTest = false;
+		SpringArm->bUsePawnControlRotation = true;
+		SpringArm->bInheritPitch = false;
+		SpringArm->bInheritRoll = true;
+		SpringArm->bInheritYaw = true;
+	}
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	if (Camera)
+	{
+		Camera->FieldOfView = 90.0f;
+		Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	}
+
+	FP_Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Mesh"));
+	if (FP_Mesh)
+	{
+		FP_Mesh->SetupAttachment(Camera);
+	}
+
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMesh"));
+	if (GunMesh)
+	{
+		GunMesh->SetupAttachment(Camera);
+	}
+
+	MaxHealth = 100.0f;
+	CurrentHealth = MaxHealth;
+	
+	bCanShoot = true;
+	bUnlimitedAmmo = false;
+	MaxAmmo = 20;
+	CurrentAmmo = MaxAmmo;
+
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (CurrentHealth != MaxHealth)
+	{
+		CurrentHealth = MaxHealth;
+	}
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent * PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	check(PlayerInputComponent);
+}
+
+void APlayerCharacter::OnDeath_Implementation()
+{
 }
 
 
--- Source/MyProject2/Public/Characters/PlayerCharacter.h
+++ Source/MyProject2/Public/Characters/PlayerCharacter.h
@@ -13,11 +13,49 @@ UCLASS()
 class MYPROJECT2_API APlayerCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+		class USpringArmComponent* SpringArm;
+
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+		class UCameraComponent* Camera;
+
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+		class USkeletalMeshComponent* FP_Mesh;
+
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+		class USkeletalMeshComponent* GunMesh;
 	
 public:
 	//Constructor
 	APlayerCharacter();
-	
-	
+
+	UFUNCTION(BlueprintPure, Category = "Character|Gun|Ammo")
+		int32 GetCurrentAmmo() const { return CurrentAmmo; }
+
+	UFUNCTION(BlueprintPure, Category = "Character|Gun|Ammo")
+		int32 GetMaxAmmo() const { return MaxAmmo; }
+
+	UFUNCTION(BlueprintCallable, Category = "Character|Gun|Ammo")
+		void SetCurrentAmmo(int NewAmmo) { CurrentAmmo = NewAmmo; }
+
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	virtual void OnDeath_Implementation() override;
+
+	int CurrentAmmo;
+
+	bool HasAmmo() const { return (CurrentAmmo > 0); }
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character|Gun")
+		bool bCanShoot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character|Gun|Ammo")
+		bool bUnlimitedAmmo;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Character|Gun|Ammo")
+		int MaxAmmo;
 	
 };
